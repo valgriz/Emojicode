bfbacf12aeeea03a2d83e7fdab0185f8816ec963
Steven <>
2017-01-13 15:37:36 -0500

    working on input bar

--- app/src/main/java/com/smarturano/emojicode/Main.java
+++ app/src/main/java/com/smarturano/emojicode/Main.java
@@ -1,7 +1,13 @@
 package com.smarturano.emojicode;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class Main extends AppCompatActivity {
 
@@ -9,5 +15,12 @@ public class Main extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        View mainView = findViewById(R.id.activity_main);
+        mainView.getRootView().setBackgroundColor(Color.parseColor("#FF5733"));
+        getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_PAN);
+        EditText input = (EditText) findViewById(R.id.input);
+        input.setSelection(0);
+        getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_VISIBLE);
+
     }
 }
